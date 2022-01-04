4bac5bf184d9626b25183bd6658ac9f7e481ad07
HectorMG02 <hectormatgon@gmail.com>
2022-01-04 21:03:47 +0100

    cargar el contrato

--- client/app.js
+++ client/app.js
@@ -1,19 +1,30 @@
 const App = {
-  web3Provider: "",
+  contracts: {},
   init: () => {
     // funciones para interactuar con la blockchain, usando metamask (instalar en el navegador)
     App.loadEthereum();
+    App.loadContracts();
   },
   loadEthereum: async () => {
-    if (!window.ethereum) {
-      alert("La extensión de metamask no esta instalada");
-    } else if (window.web3) {
-      // por si el navegador usa web3 en vez de metamask
-      const web3 = new Web3(window.web3.currentProvider);
-    } else {
+    if (window.ethereum) {
       App.web3Provider = window.ethereum;
       await window.ethereum.request({ method: "eth_requestAccounts" });
+    } else if (window.web3) {
+      // por si el navegador usa web3 en vez de metamask
+      web3 = new Web3(window.web3.currentProvider);
+    } else {
+      alert("La extensión de metamask no esta instalada");
     }
+  },
+  loadContracts: async () => {
+    const res = await fetch("TasksContract.json");
+    const tasksContractJSON = await res.json();
+
+    App.contracts.tasksContract = TruffleContract(tasksContractJSON);
+
+    App.contracts.tasksContract.setProvider(App.web3Provider);
+
+    App.tasksContract = await App.contracts.tasksContract.deployed();
   },
 };
 
