2f98d831a219fc4bd379788865b26b233f282ff5
HectorMG02 <hectormatgon@gmail.com>
2022-01-04 17:58:16 +0100

    lista de tareas

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.0;
 
 contract TaskContract {
-    // sistema de clave valor
+    struct Task {
+        uint256 id;
+        string title;
+        string description;
+        bool done;
+        uint256 createdAt; // la fecha es un timestamp, por lo que ponemos el uint256
+    }
+
+    mapping(uint256 => Task) public tasks;
 }
